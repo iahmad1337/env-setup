85bdd2698ff62f8d793fc3f78f8842f37b48aba0
Ahmad Ibrahim <iahmd01@gmail.com>
2022-12-15 01:12:44 +0300

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -eu
+
+# This script is responsible for deploying a minimal development enrironment on
+# remote machines. I only want this to work on ubuntu.
+
+echo "Starting deployment..."
+echo "Initial state of filesystem:"
+df -h
+
+if [ ! -e "ROOT_MARKER" ]; then
+    echo "This script must be run from the directory in which it was installed"
+    exit 1
+fi
+
+################################################################################
+#                        Creating common dirs and files                        #
+################################################################################
+mkdir -p ~/personal
+mkdir -p ~/.local/bin
+mkdir -p ~/.vim
+cp min-vimrc ~/.vimrc
+echo "SELECTED_EDITOR=vim" >~/.selected_editor
+
+################################################################################
+#                       Download the necessary software                        #
+################################################################################
+
+progs="vim g++ gcc python3 make mc htop ncdu"
+
+for prog in $progs; do
+    # sudo apt install -y "$prog" || echo "Failed to install $prog"
+    echo "Installed $prog"
+done
+
+echo "Deploment finished."
+echo "Current state of the filesystem:"
+df -h
---
+++ full-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+# This script is responsible for deploying a full-blown development enrironment
+# on remote machines. I only want this to work on ubuntu.
+
+# TODO: latest (or fixed) version of nvim with only some of my plugins
+# TODO: python3, fixed version of clang binaries
+# TODO: development utils (make and other stuff)
+# TODO: everything else from `deploy.sh`
+# TODO: my own bin directory (perhaps in .local) with symlinks to all the
+# binaries I need (e.g. clangd)
+# TODO: add CLANG13 and other shit to .bashrc
