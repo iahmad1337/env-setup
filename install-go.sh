ae37008a46f822e1308cea587ee777253de98c46
Ahmad Ibrahim <iahmd01@gmail.com>
2024-06-30 03:10:09 +0300

    added install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eux
+
+wget \
+    https://go.dev/dl/go1.22.4.linux-amd64.tar.gz \
+    -P ~/programs
+
+tar -C ~/programs -xzf ~/programs/go1.22.4.linux-amd64.tar.gz
+
+echo 'export PATH=$PATH:'$HOME/programs'/go/bin' >>~/.bashrc
