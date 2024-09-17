f115dcae16a4b38a29dcb68b947c30772e1899f2
Ahmad Ibrahim <iahmd01@gmail.com>
2024-09-17 17:07:03 +0300

    add git configuration

---
+++ configure-git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eux
+
+git config --global alias.st status
+git config --global alias.cam "commit -am"
+git config --global alias.cfg "config --list"
+git config --global alias.co checkout
+git config --global alias.cob checkout -b
+git config --global alias.fixup "commit -a --fixup=HEAD"
