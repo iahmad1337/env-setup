b8ea609c8b3fd90585a234547219bb74ffe88009
Ahmad I <iahmd01@gmail.com>
2024-11-07 13:32:09 +0300

    fixup! fixup! Update nvim config

--- configure-git.sh
+++ configure-git.sh
@@ -6,5 +6,5 @@ git config --global alias.st status
 git config --global alias.cam "commit -am"
 git config --global alias.cfg "config --list"
 git config --global alias.co checkout
-git config --global alias.cob checkout -b
+git config --global alias.cob "checkout -b"
 git config --global alias.fixup "commit -a --fixup=HEAD"
